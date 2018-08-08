43ca5dfdfaa4e4d0ce0ab0dee4088521928ad485
kanapuliAthavan <athavankanapuli@gmail.com>
2018-08-08 06:18:25 +0530

    add fib_test.go

---
+++ examples/fib/fib_test.go
@@ -0,0 +1,24 @@
+package fib
+
+import (
+	"testing"
+)
+
+func Fib(n int) int {
+	switch n {
+	case 0:
+		return 1
+	case 1:
+		return 1
+	case 2:
+		return 1
+	default:
+		return Fib(n-1) + Fib(n-2)
+	}
+}
+
+func BenchmarkFib(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(20)
+	}
+}
