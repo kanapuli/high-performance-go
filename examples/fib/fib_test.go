94e6b14e3405a75899f638f440b19a7d8c86cb8f
kanapuliAthavan <athavankanapuli@gmail.com>
2018-08-08 20:22:07 +0530

    Recreating the slide

--- examples/fib/fib_test.go
+++ examples/fib/fib_test.go
@@ -10,14 +10,15 @@ func Fib(n int) int {
 		return 1
 	case 1:
 		return 1
-	case 2:
-		return 1
+		//	case 2:
+		//		return 1
 	default:
 		return Fib(n-1) + Fib(n-2)
 	}
 }
 
 func BenchmarkFib(b *testing.B) {
+	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
 		Fib(20)
 	}
